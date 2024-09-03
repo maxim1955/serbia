3432b38c36d1ce4ab5a8ff515bf9d7c43603e676
Paul Belikov <paul.belikov@mail.ru>
2024-09-04 00:29:12 +0300

    Выделена модалка на восстановление пароля

---
+++ js/recovery.js
@@ -0,0 +1,156 @@
+const passModal = document.querySelector('.pass__modal-z')
+const passModalEmail = document.querySelector('.pass__container-email')
+
+const passModalCode = document.querySelector('.pass__container-code')
+
+const passModalPasswords = document.querySelector('.pass__container-pass')
+
+const passModalClose = document.querySelector('.pass__close')
+
+passModalClose.addEventListener('click', (e) => {
+  window.location.href = '/login';
+})
+
+function updateButtonVisibility(input) {
+  const button = input.nextElementSibling;
+  if (input.value.length === 0) {
+    button.classList.add("input-figure-hidden");
+  } else {
+    button.classList.remove("input-figure-hidden");
+  }
+}
+
+function clearField(input) {
+  input.value = "";
+  updateButtonVisibility(input);
+}
+
+function clearFields(input) {
+  input.value = "";
+}
+
+const inputWithClear = document.querySelectorAll(".input__clear");
+
+inputWithClear.forEach((item) => {
+  item.addEventListener("input", () => {
+    updateButtonVisibility(item);
+  });
+
+  const clearButton = item.nextElementSibling;
+  if (clearButton) {
+    clearButton.addEventListener("click", () => {
+      clearField(item);
+    });
+  }
+});
+
+let passValidation = new JustValidate('#passform', {
+  focusInvalidField: false,
+});
+
+passValidation.addField('#emailpassform', [
+  {
+    rule: 'required',
+    errorMessage: 'Введите Email'
+  },
+  {
+    rule: "email",
+    errorMessage: "Введите корректный E-mail",
+  },
+  {
+    rule: "maxLength",
+    value: 256,
+    errorMessage: "E-mail не может содержать больше 256 символов",
+  },
+])
+  .onSuccess((e) => {
+    e.preventDefault()
+    passModalEmail.style.display = "none";
+    passModalCode.style.display = "flex";
+  })
+
+let passwordsValidation = new JustValidate('#passwords', {
+  focusInvalidField: false,
+});
+
+passwordsValidation.addField('#newpass-passform', [
+  {
+    rule: 'required',
+    errorMessage: 'Введите новый пароль'
+  },
+  {
+    rule: "maxLength",
+    value: 64,
+    errorMessage: "Новый пароль не может содержать больше 64 символов",
+  },
+  {
+    rule: "minLength",
+    value: 6,
+    errorMessage: "Новый пароль не может содержать меньше 6 символов",
+  }
+])
+  .addField('#repnewpass-passform', [
+    {
+      rule: 'required',
+      errorMessage: 'Повторите новый пароль'
+    },
+    {
+      validator: (value, fields) => {
+        if (
+          fields['#newpass-passform'] &&
+          fields['#repnewpass-passform'].elem
+        ) {
+          const repeatPasswordValue =
+            fields['#newpass-passform'].elem.value;
+
+          return value === repeatPasswordValue;
+        }
+
+        return true;
+      },
+      errorMessage: 'Пароли не совпадают',
+    }
+  ])
+  .onSuccess((e) => {
+    e.preventDefault()
+    passModalPasswords.style.display = "none";
+    window.location.href = '/login';
+  })
+
+let codeValidation = new JustValidate('#codeform', {
+  focusInvalidField: false,
+});
+
+codeValidation.addField('#code', [
+  {
+    rule: 'required',
+    errorMessage: ' '
+  },
+  {
+    rule: 'minLength',
+    value: 10,
+    errorMessage: "Код содержит минимум 10 симоволов",
+  }
+])
+  .onSuccess((e) => {
+    e.preventDefault()
+    passModalCode.style.display = "none";
+    passModalPasswords.style.display = "flex";
+  })
+
+
+const passwordBtns = document.querySelectorAll(".input-figure-eye");
+const inputPasswords = document.querySelectorAll(".input__pass");
+
+for (let i = 0; i < passwordBtns.length; i++) {
+  passwordBtns[i].addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const inputPassword = inputPasswords[i];
+    if (inputPassword.getAttribute("type") === "password") {
+      inputPassword.setAttribute("type", "text");
+    } else {
+      inputPassword.setAttribute("type", "password");
+    }
+  });
+}
